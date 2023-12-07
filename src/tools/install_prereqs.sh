fecea78da5fa9f96121deac271044be185226700
Oscar Moll <orm@csail.mit.edu>
2023-12-07 16:22:02 -0500

    change location

--- src/tools/install_prereqs.sh
+++
@@ -1,7 +0,0 @@
-## check if this is ubuntu, and libreadline-dev is not installed, then install it/
-if [ -f /etc/lsb-release ]; then
-    if ! dpkg -s libreadline-dev >/dev/null 2>&1; then
-        sudo apt-get update
-        sudo apt-get install -y libreadline-dev
-    fi
-fi
