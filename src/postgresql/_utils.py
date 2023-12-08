3eed439cfd0499acf255f25f789fd3876e288406
Oscar Moll <orm@csail.mit.edu>
2023-12-08 16:40:45 -0500

    avoid stderr msg in the common case

--- src/postgresql/_utils.py
+++ src/postgresql/_utils.py
@@ -10,12 +10,17 @@ __all__ = ["TemporaryPostgres"]
 
 class TemporaryPostgres:
     """
-        Temporary postgresql server instance for testing.
+        Temporary postgresql server instance which can be used
+        as context manager.
         Listens on unix-domain socket to avoid port conflicts.
-        Provides context manager for stopping the server and removing the pgdata directory.
-        It will also stop the server and remove the pgdata directory when the process ends.
-        NOTE: at the moment the server and data directory won't be automatically 
-        removed if the process is killed.
+        cleanup of server and data can be managed via:
+
+        1) context manager interface
+        2) manual call to teardown()
+        3) atexit handler if neither of the above are used
+
+        NOTE: at the moment the server and data directory will not be automatically 
+        removed if the process is killed with SIGKILL or SIGTERM (see atexit doc)
         
         Args:
             dir: base directory within which temporary pgdata directory will be created.
@@ -29,20 +34,23 @@ class TemporaryPostgres:
         self.socket_dir = self.pgdata # for now, we'll use the pgdata directory for the socket
 
         initdb(f"-D {self.pgdata} --auth=trust --auth-local=trust --no-sync -U {self.user}")
-        atexit.register(self.teardown)
+        atexit.register(self._teardown)
         pg_ctl(f'-D {self.pgdata} -w -o "-k {self.socket_dir} -h \\"\\"" -l {self.log} start')
 
-    def teardown(self) -> None:
+    def _teardown(self) -> None:
         """ Stops the postgresql server and removes the pgdata directory. """
         try:
-            print('shutting down server')
             pg_ctl(f"-D {self.pgdata} -m immediate stop")
-        except CalledProcessError: # may already have stopped, or never started. this is fine.
-            print('server already stopped')
+        except CalledProcessError: 
+            pass
 
         if self.pgdata.exists():
             shutil.rmtree(str(self.pgdata))
-        print('done with teardown')
+
+    def teardown(self) -> None:
+        """ Stops the postgresql server and removes the pgdata directory. """
+        self._teardown()
+        atexit.unregister(self._teardown) # avoid stderror from double cleanup 
     
     def get_connection_uri(self, database : Optional[str] = None) -> str:
         """ Returns a connection string for the postgresql server.
@@ -53,6 +61,9 @@ class TemporaryPostgres:
         return f"postgresql://{self.user}:@/{database}?host={self.socket_dir}"
 
     def run_psql_command(self, sql : str) -> str:
+        """ 
+            TODO: pass from stdin instead of using -c
+        """
         return psql(f"-d {self.get_connection_uri()} -c '{sql}'")
         
     def get_status(self):
@@ -62,20 +73,26 @@ class TemporaryPostgres:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.teardown()
+        self._teardown()
+        atexit.unregister(self._teardown)
 
 
-class PostgresInstance:
+class Postgres:
     """ Represents a local postgres instance where the pgdata is not temporary.
         If the pgdata directory does not exist, it will be created.
         If the server is not running, it will be started.
         If the pgdata directory exists, it will be used as-is.
-        If this is the last process using the database, the server process will be stopped.
+
+        Uses refcounts to determine when to stop the server process.
+        If this is the last process using the database, the server process will be stopped on teardown(),
+        New instances will restart the server.
     """
-    def __init__(self, pgdata : Path, ):
+    def __init__(self, pgdata : Path):
         self.user = "postgres"
         self.pgdata = pgdata
         self.log = self.pgdata / 'log'
+        self.socket_dir = self.pgdata
+
 
 
 
