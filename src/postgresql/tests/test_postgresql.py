932f4d25b5a8d923de4d576b9cdcc0bcba9dc03a
Oscar Moll <orm@csail.mit.edu>
2023-12-10 17:01:22 -0500

    disambiguate tests

--- src/postgresql/tests/test_postgresql.py
+++ src/postgresql/tests/test_postgresql.py
@@ -2,14 +2,15 @@ import pytest
 from postgresql import PostgresServer
 import subprocess
 import tempfile
+import multiprocessing as mp
 
 
 def _check_server_works(pg : PostgresServer) -> int:
     assert pg.pgdata.exists()
     pid = pg.get_pid()
     assert pid is not None
-    ret = pg.psql("select version();")
-    assert ret.startswith("PostgreSQL")
+    ret = pg.psql("show data_directory;")
+    assert str(pg.pgdata) in ret
     return pid
 
 def _server_is_running(pid : int) -> bool:
@@ -22,13 +23,15 @@ def _server_is_running(pid : int) -> bool:
     return False
 
 def _kill_server(pid : int) -> None:
-    if pid is not None:
-        subprocess.run(["kill", "-9", str(pid)], check=False)
+    if pid is None:
+        return
+    subprocess.run(["kill", "-9", str(pid)])
 
 def test_cleanup_default():
     with tempfile.TemporaryDirectory() as tmpdir:
+        pid = None
         try:
-            with PostgresServer.get_server(tmpdir.name) as pg:
+            with PostgresServer.get_server(tmpdir) as pg:
                 pid = _check_server_works(pg)
 
             assert not _server_is_running(pid)
@@ -36,10 +39,70 @@ def test_cleanup_default():
         finally:
             _kill_server(pid)
 
+def test_reentrant():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        pid = None
+        try:
+            with PostgresServer.get_server(tmpdir) as pg:
+                pid = _check_server_works(pg)
+                with PostgresServer.get_server(tmpdir) as pg2:
+                    assert pg2 is pg
+                    _check_server_works(pg)
+
+                _check_server_works(pg)
+
+            assert not _server_is_running(pid)
+            assert pg.pgdata.exists()
+        finally:
+            _kill_server(pid)
+
+def _start_and_wait(tmpdir, queue_in, queue_out):
+    import postgresql
+    with postgresql.PostgresServer.get_server(tmpdir) as pg:
+        pid = _check_server_works(pg)
+        queue_out.put(pid)
+
+        # now wait for parent to tell us to exit
+        _ = queue_in.get()
+
+def test_multiprocess_shared():
+    """ Test that multiple processes can share the same server
+        by getting server in a child process, 
+        then getting it again in the parent process.
+        then exiting the child process.
+        Then checking that the parent can still use the server.
+    """
+    try:
+        pid = None
+        with tempfile.TemporaryDirectory() as tmpdir:
+            queue_to_child = mp.Queue()
+            queue_from_child = mp.Queue()
+            child = mp.Process(target=_start_and_wait, args=(tmpdir,queue_to_child,queue_from_child))
+            child.start()
+            # wait for child to start server
+            server_pid_child = queue_from_child.get()
+
+            with PostgresServer.get_server(tmpdir) as pg:
+                server_pid_parent = _check_server_works(pg)
+                assert server_pid_child == server_pid_parent
+
+                # tell child to continue
+                queue_to_child.put(None)
+                child.join()
+            
+                # check server still works
+                _check_server_works(pg)
+
+            assert not _server_is_running(server_pid_parent)
+    finally:
+        _kill_server(pid)                
+
+
 def test_cleanup_delete():
     with tempfile.TemporaryDirectory() as tmpdir:
+        pid = None
         try:
-            with PostgresServer.get_server(tmpdir.name, cleanup_mode='delete') as pg:
+            with PostgresServer.get_server(tmpdir, cleanup_mode='delete') as pg:
                 pid = _check_server_works(pg)
 
             assert not _server_is_running(pid)
@@ -49,8 +112,9 @@ def test_cleanup_delete():
 
 def test_cleanup_none():
     with tempfile.TemporaryDirectory() as tmpdir:
+        pid = None
         try:
-            with PostgresServer.get_server(tmpdir.name, cleanup_mode=None) as pg:
+            with PostgresServer.get_server(tmpdir, cleanup_mode=None) as pg:
                 pid = _check_server_works(pg)
 
             assert _server_is_running(pid)
@@ -65,5 +129,5 @@ def tmp_postgres():
         yield pg
 
 def test_pgvector(tmp_postgres):
-    ret = tmp_postgres.run_psql_command("CREATE EXTENSION vector;")
+    ret = tmp_postgres.psql("CREATE EXTENSION vector;")
     assert ret == "CREATE EXTENSION\n"
