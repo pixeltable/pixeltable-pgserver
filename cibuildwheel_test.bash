b5de3ecfe1755ea8488f6d02c814c5f8ea237f21
Oscar Moll <orm@csail.mit.edu>
2024-04-04 00:56:36 -0400

    re-add skip on manylinux

--- cibuildwheel_test.bash
+++ cibuildwheel_test.bash
@@ -2,5 +2,12 @@
 PROJECT=$1
 
 echo "Running on OSTYPE=$OSTYPE with UID=$UID"
-pytest -v $PROJECT/tests
 
+case "$OSTYPE" in
+    linux*)
+        echo "Tests disabled on the manylinux container bc still debugging running in those environment constraints"
+        ;;
+    *)
+        pytest -v $PROJECT/tests
+        ;;
+esac
