63c1b80b5e7a9672bab8e74a0853e76bb9afbedd
Oscar Moll <orm@csail.mit.edu>
2024-04-04 18:37:59 -0400

    also enable linux

--- cibuildwheel_test.bash
+++ cibuildwheel_test.bash
@@ -4,9 +4,9 @@ PROJECT=$1
 echo "Running on OSTYPE=$OSTYPE with UID=$UID"
 
 case "$OSTYPE" in
-    linux*)
-        echo "Tests disabled on the manylinux docker container: still debugging test failures only in this environment"
-        ;;
+    # linux*)
+    #     echo "Tests disabled on the manylinux docker container: still debugging test failures only in this environment"
+    #     ;;
     *)
         pytest -s -v --log-cli-level=INFO $PROJECT/tests
         ;;
