ef308b5b3bfd4235a7fa4af4c26de2ed43cc5823
Oscar Moll <orm@csail.mit.edu>
2024-04-04 09:46:35 -0400

    re-enable linux

--- cibuildwheel_test.bash
+++ cibuildwheel_test.bash
@@ -1,13 +1,5 @@
 #! /bin/bash
 PROJECT=$1
 
-echo "Running on OSTYPE=$OSTYPE with UID=$UID"
-
-case "$OSTYPE" in
-    linux*)
-        echo "Tests disabled on the manylinux container bc still debugging running in those environment constraints"
-        ;;
-    *)
-        pytest -v $PROJECT/tests
-        ;;
-esac
+echo "Running tests on OSTYPE=$OSTYPE with UID=$UID"
+pytest -v $PROJECT/tests
