885995b03cd184692c1363faadf7c5624a400a52
Oscar Moll <orm@csail.mit.edu>
2024-04-22 23:24:41 -0400

    test

--- cibuildwheel_test.bash
+++ cibuildwheel_test.bash
@@ -4,9 +4,9 @@ PROJECT=$1
 echo "Running on OSTYPE=$OSTYPE with UID=$UID"
 
 case "$OSTYPE" in
-    linux*)
-        echo "Tests disabled on the manylinux docker container for now"
-        ;;
+    # linux *)
+    #     echo "Tests disabled on the manylinux docker container for now"
+    #     ;;
     *)
         pytest -s -v --log-cli-level=INFO $PROJECT/tests
         ;;
