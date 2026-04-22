6dc9aef4da2475453202e8b8cbe180ee447feece
Christopher Pestano <cpestano@pixeltable.com>
2026-04-22 15:42:26 -0700

    Making windows happy

--- src/pixeltable_pgserver/postgres_server.py
+++ src/pixeltable_pgserver/postgres_server.py
@@ -142,13 +142,16 @@ class PostgresServer:
                         proc.kill()
                     assert not proc.is_running()
 
+            # Windows' libc doesn't accept "C.UTF-8"; UCRT (Windows 10 1803+) does accept
+            # ".UTF-8", which uses the host's default language with the UTF-8 codepage.
+            locale = '.UTF-8' if platform.system() == 'Windows' else 'C.UTF-8'
             pgexec(
                 'initdb',
                 (
                     '--auth=trust',
                     '--auth-local=trust',
                     '--encoding=utf8',
-                    '--locale=C.UTF-8',
+                    f'--locale={locale}',
                     '-U',
                     self.postgres_user,
                     '-D',
